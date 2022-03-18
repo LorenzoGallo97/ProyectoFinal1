e53944c61ccf5b653a10b0da8e49d2315aec112c
LorenzoGallo97 <92406178+LorenzoGallo97@users.noreply.github.com>
2022-03-17 21:33:03 -0300

    Add files via upload

---
+++ Json/desafio2.js
@@ -0,0 +1,13 @@
+let formularios = {
+    vendedor: "Sandra",
+    edad: 35,
+    ocupacion: Gestor
+  };
+  const setearPropiedad = (propiedad, valor) => {
+    persona[propiedad] = valor;
+  };
+  const papel = {
+    tipo: "04",
+    vehiculo: "Auto",
+  };
+  console.log(vendedor, edad, ocupacion);
